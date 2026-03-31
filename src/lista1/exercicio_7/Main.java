b6912e46e824a620df9a91f4dbab2311c4efa2c8
Feliipee013 <ftaveira611@gmail.com>
2026-03-30 21:15:30 -0300

    Exercício 7 finalizado

--- src/lista1/exercicio_7/Main.java
+++ src/lista1/exercicio_7/Main.java
@@ -4,11 +4,13 @@ public class Main {
     public static void main(String[] args) {
         String senha1 = "senha12345";
         String senha2 = "senha";
-        String senha3 = "1234541241241";
+        String senha3 = "0123456789";
+        String senha4 = "0123456789a";
 
         Autenticador autenticador = new Autenticador();
         autenticador.validar(senha1);
         autenticador.validar(senha2);
         autenticador.validar(senha3);
+        autenticador.validar(senha4);
     }
 }

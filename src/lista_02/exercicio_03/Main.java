492a33b233ea924aef3b9befc1ae0af8564bd52d
Feliipee013 <ftaveira611@gmail.com>
2026-04-21 13:19:22 -0300

    modificao no exercicio 3

--- src/lista_02/exercicio_03/Main.java
+++ src/lista_02/exercicio_03/Main.java
@@ -4,15 +4,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
         Menu menu = new Menu();
-
-        int opcao = -1;
-        do {
-            menu.exibirMenu();
-            opcao = sc.nextInt();
-        } while(opcao != 0);
-
     }
 }
--- src/lista_02/exercicio_03/Menu.java
+++ src/lista_02/exercicio_03/Menu.java
@@ -1,7 +1,14 @@
 package lista_02.exercicio_03;
 
+import java.util.Scanner;
+
 public class Menu {
     public void exibirMenu() {
-        System.out.println("Digite uma opção:\n1 - Coxinha\n2 - Pastel\n3 - Bolo\n0 - Sair");
+        Scanner sc = new Scanner(System.in);
+        int opcao = -1;
+        do {
+            System.out.println("Digite uma opção:\n1 - Coxinha\n2 - Pastel\n3 - Bolo\n0 - Sair");
+            opcao = sc.nextInt();
+        } while(opcao != 0);
     }
 }

268ff4aeb39ce0e3329add3ff66bf58fa4e3ca2f
JF532 <jfilipe532@hotmail.com>
2026-04-27 12:49:47 -0300

    Exercício 6 - Lista 3

---
+++ src/lista_03/exercicio_06/Main.java
@@ -0,0 +1,9 @@
+package lista_03.exercicio_06;
+
+public class Main {
+    public static void main(String[] args) {
+        PassagemArray passagemArray = new PassagemArray();
+        int arranjo[] = {2,3,4,5,2,3,6};
+        passagemArray.multiplicarValores(arranjo);
+    }
+}

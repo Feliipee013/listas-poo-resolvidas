cea5bdec989d92b3b69c2f65a695799ba400d249
JF532 <jfilipe532@hotmail.com>
2026-04-27 18:09:56 -0300

    Exercício 10 - Lista 4

---
+++ src/lista_04/exercicio_10/Main.java
@@ -0,0 +1,8 @@
+package lista_04.exercicio_10;
+
+public class Main {
+    public static void main(String[] args) {
+        PegadinhaConstrutor pegadinhaConstrutor = new PegadinhaConstrutor();
+
+    }
+}
